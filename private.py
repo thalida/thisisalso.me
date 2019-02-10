3db23da9c691c11963c6eec7a24ba2c6e14fc49f
Thalida Noel <thalida.c.noel@gmail.com>
2019-02-10 02:45:45 -0500

    Added test endpoints and databases

--- private.py
+++ private.py
@@ -2,15 +2,37 @@ import os
 os.environ['TZ'] = 'UTC'
 
 import logging
+import psycopg2
 from pprint import pprint
 from flask import Flask, request, render_template, make_response, jsonify, abort
 
+# POST
+# id = PK
+# parent_id = FK
+# contents = TEXT
+# theme = TEXT
+# timestamp = DATETIME (w/o timezone)
+# (now() at time zone 'utc')
+
 logger = logging.getLogger(__name__)
+db = psycopg2.connect("dbname=thisisalsome user=thalida")
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('private/index.html')
+
+@app.route('/api/post/create', methods=['POST'])
+def api_create():
+    try:
+        with psycopg2.connect("dbname=thisisalsome user=thalida") as conn:
+            with conn.cursor() as cur:
+                cur.execute("INSERT INTO post (id, contents, theme) VALUES (%s, %s, %s)", (3, 'version testing one more time', '',))
+                cur.execute("INSERT INTO post (contents, theme) VALUES (%s, %s)", ('foobarbatcat', '#ff4848',))
+        return make_response(jsonify({'status': 'success'}))
+    except Exception:
+        logger.exception('500 Error Saving Post')
+        abort(500)
 
 @app.route('/api/post/save', methods=['POST'])
 def api_save():
